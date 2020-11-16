2d2a968b1b2d79fc3a6a826ddf52cfb06942ae6e
eduardo <domingues.m.eduardo@gmail.com>
2020-11-16 10:03:06 +0000

    send emails

--- src/components/CalendarHeatmap.vue
+++ src/components/CalendarHeatmap.vue
@@ -59,9 +59,9 @@
 </template>
 
 <script>
-import { VTooltip } from 'v-tooltip'
+import {VTooltip} from 'v-tooltip'
 import Heatmap from './Heatmap'
-import { DAYS_IN_WEEK, DEFAULT_LOCALE, DEFAULT_RANGE_COLOR, DEFAULT_TOOLTIP_UNIT, SQUARE_SIZE } from './consts.js'
+import {DAYS_IN_WEEK, DEFAULT_LOCALE, DEFAULT_RANGE_COLOR, DEFAULT_TOOLTIP_UNIT, SQUARE_SIZE} from './consts.js'
 
 VTooltip.enabled = window.innerWidth > 768
 
